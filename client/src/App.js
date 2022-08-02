043f8d6732af75cd6b62d22bda8bc7b0a0d9065c
qkransrb <qkransrb90@gmail.com>
2022-08-02 23:22:37 +0900

    modify client

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-import About from "../pages/About";
-import Home from "../pages/Home";
+import About from "./pages/About";
+import Home from "./pages/Home";
 
 const App = () => {
   return (
